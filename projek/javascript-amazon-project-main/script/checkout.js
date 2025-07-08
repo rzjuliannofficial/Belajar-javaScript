1afb45e430cd15a7b36a10509982ea60bbd326a7
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-07-08 16:06:51 +0700

    Generate HTML for the order summary

--- projek/javascript-amazon-project-main/script/checkout.js
+++ projek/javascript-amazon-project-main/script/checkout.js
@@ -1,6 +1,8 @@
 import {cart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
+let cartSummaryHTML = '';
+
 cart.forEach((cartItem) => {
 
     const productId = cartItem.productId;
@@ -16,7 +18,7 @@ cart.forEach((cartItem) => {
     // console.log(matchingProduct);
 
 
-    `
+    cartSummaryHTML += `
     <div class="cart-item-container">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
@@ -94,3 +96,6 @@ cart.forEach((cartItem) => {
     </div>
     `
 });
+
+document.querySelector('.js-order-summary')
+    .innerHTML = cartSummaryHTML;
